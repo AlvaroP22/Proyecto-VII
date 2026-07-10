547bd3bf82c6f8f9968563e10c45ea57187fb94e
sofiafgm <sofia.fgm@gmail.com>
2026-07-10 17:44:34 -0600

    fix: button on homes banner

--- cliente/src/app/pages/home/home.component.ts
+++ cliente/src/app/pages/home/home.component.ts
@@ -1,14 +1,15 @@
-import { Component, NgModule } from '@angular/core';
+import { Component } from '@angular/core';
 import { LoginComponent } from '../login/login.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LoginComponent, CommonModule, FormsModule, HttpClientModule],
+  imports: [LoginComponent, CommonModule, FormsModule, HttpClientModule, RouterLink],
   providers: [HttpClient],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
